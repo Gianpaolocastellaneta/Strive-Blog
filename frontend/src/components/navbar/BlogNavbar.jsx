b7d281117539fe827ee7112881ecfaa6b1bbe7a5
Gianpaolocastellaneta <gianp95@gmail.com>
2024-09-08 20:40:14 +0200

    edit response/toggle menu BlogNavBar

--- frontend/src/components/navbar/BlogNavbar.jsx
+++ frontend/src/components/navbar/BlogNavbar.jsx
@@ -61,32 +61,63 @@ const NavBar = props => {
         <Navbar.Brand as={Link} to="/">
           <img className="blog-navbar-brand" alt="logo" src={logo} />
         </Navbar.Brand>
+
+        {/* Toggle menu small */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="d-lg-none" />
+
+        {/* Collapsible menu */}
+        <Navbar.Collapse id="basic-navbar-nav" className="d-lg-none">
+          {token && (
+            <div className="d-flex flex-column w-100">
+              <Button as={Link} to="/new" className="blog-navbar-add-button bg-dark mb-3 mt-4 d-lg-none">
+                + Add New Post
+              </Button>
+              <Button className="d-lg-none w-25" variant="primary" onClick={handleLogout}>
+                Logout
+              </Button>
+            </div>
+          )}
+          {authorInfo && <Image src={authorInfo.avatar} className="authorAvatar mt-3 d-lg-none" />}
+        </Navbar.Collapse>
+
+        {/* Navigation medium large */}
+        <div className="d-none d-lg-flex">
+          {!token && (
+            <Button className="ms-3" variant="dark" onClick={handleShowReg}>
+              Register
+            </Button>
+          )}
+          {token && (
+            <>
+              <Button as={Link} to="/new" className="blog-navbar-add-button bg-dark mx-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-plus-lg"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
+                </svg>
+                Add New Post
+              </Button>
+              <Button className="ms-5 me-2" variant="primary" onClick={handleLogout}>
+                Logout
+              </Button>
+            </>
+          )}
+          {authorInfo && <Image src={authorInfo.avatar} className="authorAvatar" />}
+        </div>
+
+       
         <Modal className="bg-secondary" show={showReg} onHide={handleCloseReg}>
           <Modal.Header closeButton className="bg-dark">
             <Modal.Title className="bg-dark text-light">REGISTER YOUR ACCOUNT</Modal.Title>
           </Modal.Header>
           <Modal.Body className="bg-dark">
-            <Form className=" px-5 py-5 rounded bg-dark">
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput3">
-                <Form.Label className="text-light">Email</Form.Label>
-                <Form.Control type="email" name="email" value={regFormValue.email} onChange={handleChangeRegistration} placeholder="name@example.com" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
-                <Form.Label className="text-light">Password</Form.Label>
-                <Form.Control type="password" name="password" value={regFormValue.password} onChange={handleChangeRegistration} placeholder="password" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput5">
-                <Form.Label className="text-light">Name</Form.Label>
-                <Form.Control type="name" name="name" value={regFormValue.name} onChange={handleChangeRegistration} placeholder="name" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput6">
-                <Form.Label className="text-light">Surname</Form.Label>
-                <Form.Control type="surname" name="surname" value={regFormValue.surname} onChange={handleChangeRegistration} placeholder="surname" />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput7">
-                <Form.Label className="text-light">Profile Photo</Form.Label>
-                <Form.Control type="file" name="avatar" onChange={handleChangeImage} placeholder="your picture" />
-              </Form.Group>
+            <Form className="px-5 py-5 rounded bg-dark">
+             
             </Form>
           </Modal.Body>
           <Modal.Footer className="d-flex justify-content-between bg-dark">
@@ -98,39 +129,7 @@ const NavBar = props => {
             </Button>
           </Modal.Footer>
         </Modal>
-        <div className="d-flex">
-          {!token && <Button className="ms-3" variant="dark" onClick={handleShowReg}>
-            Register
-          </Button>}
-          {/* {token && <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search post"
-              className="me-2"
-              aria-label="Search"
-            // onChange={handleSearch}
-            />
-          </Form>} */}
-          {token && <Button as={Link} to="/new" className="blog-navbar-add-button bg-dark mx-5" >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-plus-lg"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
-            </svg>
-            Add New Post
-          </Button>}
-          {token && <Button className="ms-5 me-2" variant="primary" onClick={handleLogout}>
-            Logout
-          </Button>}
-          {authorInfo && <Image src={authorInfo.avatar} className="authorAvatar" />}
-        </div>
       </Container>
-
     </Navbar>
   );
 };
