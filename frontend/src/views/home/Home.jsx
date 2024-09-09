7322de46ee436cb0c0c599d7d502efa4afb34efa
Gianpaolocastellaneta <gianp95@gmail.com>
2024-09-09 17:15:04 +0200

    last edit button google responsive sm

--- frontend/src/views/home/Home.jsx
+++ frontend/src/views/home/Home.jsx
@@ -73,45 +73,19 @@ const Home = props => {
       <Button variant="primary" onClick={handleLogin}>
       Sign in
       </Button>
-      <Button variant="primary"className="ms-3 px-3" as={Link} to={`${process.env.REACT_APP_API_URL}/auth/login-google`}>
+      <Button variant="primary"className="ms-3 px-3 responsive-margin" as={Link} to={`${process.env.REACT_APP_API_URL}/auth/login-google`}>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
   <path d="M15.545 6.558a9.4 9.4 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.7 7.7 0 0 1 5.352 2.082l-2.284 2.284A4.35 4.35 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.8 4.8 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.7 3.7 0 0 0 1.599-2.431H8v-3.08z"/>
 </svg>
   &nbsp; Login with Google
       </Button>
     </Form>
-        {/* <Button variant="primary" className="me-3" onClick={handleShow}>
-        Login
-      </Button> or */}
+      
       
     
       </div>}
 
-      {/* <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>LOGIN</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" name="email" onChange={handleChange} placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" name="password" onChange={handleChange} placeholder="your password" />
-      </Form.Group>
-      </Form>
-      </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleLogin}>
-            Login now
-          </Button>
-        </Modal.Footer>
-      </Modal> */}
+     
      {token && <BlogList />}
     </Container>
   );
